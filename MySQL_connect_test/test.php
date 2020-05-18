44c0eb05d4f8e1230b4b6b6501ae5df3d358ded3
Shannon <js0121828@gmail.com>
2020-05-18 18:52:28 +0800

    test.php上傳

--- MySQL_connect_test/test.php
+++ MySQL_connect_test/test.php
@@ -1,8 +1,8 @@
 <?php
-        $db_link = mysql_connect("127.0.0.1", "root", "123456");
+        $db_link = mysqli_connect("127.0.0.1", "root-1", "test1234");
         if(!$db_link) {
                 echo "MySQL 伺服器連結失敗";}
         else {
                 echo "MySQL 伺服器連結成"; }
-        mysql_close($db_link);
+        mysqli_close($db_link);
 ?>

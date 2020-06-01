407d1ac1fb4085cece2f65d0cebb1823daa406ef
hsingchi chang <super20110505@gmail.com>
2020-06-01 18:53:05 +0800

    php update

--- Code/Login.php
+++ Code/Login.php
@@ -7,12 +7,14 @@
             $sql =  "SELECT s_password FROM student WHERE s_id = '$id' AND s_password = '$password'";
             $result = mysqli_query($con,$sql);
         if($result->num_rows > 0){
-            header("refresh:0;url=Main.html");
+            header("refresh:0; url = Main.html");
             exit;
         }else{
+            header("refresh:0; url = Login.html");
             echo '<script>alert("使用者名稱或密碼錯誤")</script>' ;
         }
         }else{
+            header("refresh:0; url = Login.html");
             echo '<script>alert("表單填寫不完整")</script>';
         }
     }

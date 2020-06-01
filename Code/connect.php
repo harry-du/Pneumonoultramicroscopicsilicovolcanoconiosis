554e08b05a8a7abc5fe729a3820db5f9222a2e14
hsingchi chang <super20110505@gmail.com>
2020-06-01 18:50:57 +0800

    html php update

--- Code/connect.php
+++ Code/connect.php
@@ -6,5 +6,5 @@ $con = mysqli_connect($server,$db_username,$db_password);
 if(!$con){
     die("can't connect".mysql_error());
 }
-mysqli_select_db($con, 'test');
+mysqli_select_db($con, 'chooselesson');
 ?>

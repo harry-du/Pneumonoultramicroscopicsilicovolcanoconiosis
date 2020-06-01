c80da509821100fd6581291a74e42a6f019b2fe5
hsingchi chang <super20110505@gmail.com>
2020-06-01 17:33:27 +0800

    html php update

--- Code/connect.php
+++ Code/connect.php
@@ -1,10 +1,10 @@
 <?php
-$server="localhost";//主機
-$db_username="Super";//你的資料庫使用者名稱
-$db_password="D0753008";//你的資料庫密碼
-$con = mysqli_connect($server,$db_username,$db_password);//連結資料庫
+$server = "localhost";
+$db_username = "Super";
+$db_password = "D0753008";
+$con = mysqli_connect($server,$db_username,$db_password);
 if(!$con){
-die("can't connect".mysql_error());//如果連結失敗輸出錯誤
+    die("can't connect".mysql_error());
 }
 mysqli_select_db($con, 'test');
 ?>

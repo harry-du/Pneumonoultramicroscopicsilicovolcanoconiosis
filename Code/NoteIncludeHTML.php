439283cbbeedf6f7c17aa4338bbceadc00f5fe6a
Hsu_Chun_Wei <super20110505@gmail.com>
2020-06-06 15:18:10 +0800

    php update

--- Code/NoteIncludeHTML.php
+++ Code/NoteIncludeHTML.php
@@ -33,9 +33,8 @@
     $sql1 =  "SELECT comments FROM `message`;"; 
     $result = mysqli_query($con,$sql1) or die("Query Error");
     $total_fields=mysqli_num_fields($result);
-    for ($i = 0; $i < $total_records; $i++) {
-        $row = mysqli_fetch_assoc($result){
-            echo"<textarea> $row[\"comments\"] </textarea>"
-        }
+    $row = mysqli_fetch_array($result);
+    for ($i = 0; $i < $total_fields; $i++) {
+        echo"<textarea>".$row['comments']. "</textarea>";
     }
 ?>
--- Code/Notes.php
+++ Code/Notes.php
@@ -1,13 +1,13 @@
 <?php
-    echo $_POST["notes"];
     if(isset($_POST['notes'])){
         include('connect.php');
         $sqls = "SET FOREIGN_KEY_CHECKS = 0";
         mysqli_query($con, $sqls); 
         $note = $_POST['notes'];
         if (isset($note)){
-            $sql =  "INSERT INTO `message` (`c_id`, `t_id`, `comments`) VALUES(\"1251\", \"T1428\", \"$note\")";
+            $sql =  "INSERT INTO message (`c_id`, `t_id`, `comments`) VALUES(\"1251\", \"T1428\", \"$note\")";
             $result = mysqli_query($con,$sql) or die("Query Error");
         }
     }
+    header("refresh:0; url = NoteIncludeHTML.php");
 ?>

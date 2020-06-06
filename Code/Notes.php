ec54ebd05f2ca918f2a9e5cdb5465a5c2cd13037
Hsu_Chun_Wei <super20110505@gmail.com>
2020-06-06 22:19:08 +0800

    php update

--- Code/Notes.php
+++ Code/Notes.php
@@ -1,10 +1,10 @@
 <?php
+    include('connect.php');
+    $sqls = "SET FOREIGN_KEY_CHECKS = 0";
+    mysqli_query($con, $sqls); 
     if(isset($_POST['notes'])){
-        include('connect.php');
-        $sqls = "SET FOREIGN_KEY_CHECKS = 0";
-        mysqli_query($con, $sqls); 
         $note = $_POST['notes'];
-        if ($note != ""){
+        if ($note != " "){
             $sql1 =  "INSERT INTO message (`comments`) VALUES('$note')";
             $result2 = mysqli_query($con,$sql1) or die("Query Error");
             echo '<script>alert("留言成功")</script>';

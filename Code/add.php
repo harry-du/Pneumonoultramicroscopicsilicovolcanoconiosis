336f079f2c094c4b72418e9c80780e7da82ff10f
John Doe <johndoe@example.com>
2020-06-07 15:23:00 +0800

    add improve

--- Code/add.php
+++ Code/add.php
@@ -3,41 +3,57 @@
     // if(!isset($_POST["submit"])){
     //     exit("錯誤執行");
     // }//檢測是否有submit操作 
-    require('connect.php');
+    include('connect.php');
     include('login.php');
     
-    $id = $_POST['id'];
-    $c_id = $_POST['c_id'];
-    $credit = $_POST['credit'];
-    $passowrd = $_POST['password'];
+    $id ="D0606"; //$_POST['id'];
+    $c_id = "1274";//$_POST['c_id'];
+    $credit =(int) "3";//$_POST['credit'];
+    //$passowrd = $_POST['password'];
 
     $sql1 = " Select  SUM(credit) FROM registration JOIN course WHERE s_id = '$id' AND course.c_id = registration.c_id";//"select s_id,sum(credit) as t_credit from registration group by s_id";
     $sql3 = " Select (c_id, week, time) FROM time JOIN registration WHERE $c_id<> AND week = week AND time = time";
-    $sql4 = " Select now_member FROM course WHERE c_id = '$c_id'";
-    $sql5 =  " Select max_member FROM course WHERE c_id = '$c_id'";
-    $result = mysqli_query($sql1,$con);
-    $result = mysqli_query($sq3,$con);
-    $result = mysqli_query($sq4,$con);
+    $sql4 = " Select now_member max_member FROM course WHERE c_id = '$c_id'";
+    //$sql5 = " Select max_member FROM course WHERE c_id = '$c_id'";
+    //$sql6 = " Select ";
+    
+    $result1 = mysqli_query($con,$sql1);
+    $result3 = mysqli_query($con,$sql3);
+    $result4 = mysqli_query($con,$sql4);
+    if(!$result4)
+	{
+		echo ("Error: ".mysqli_error($con));
+		exit();
+	}
+	//
+    //$result5 = mysqli_query($con,$sql5);
+    //$result5 = mysqli_query($sq6,$con);
+    $row1 = mysqli_fetch_assoc($result4);
+    $now_member =intval($row1['now_member']);
+    $max_member =intval($row1['max_member']);
+    $row2 = mysqli_fetch_assoc($result1);
+    $SUM_credit = intval($row2['SUM(credit)']);
+
+    echo "<script>alert('$max_member $now_member')</script>";
+    
     if ($id!=null){
-        if(mysqli_fetch_row($sql4)+1>mysqli_fetch_row($sql5)){
+        if($now_member+1>$max_member){
             echo "<script>alert('人數已滿')</script>";
         }
-        else if($credit+mysqli_fetch_row($sql1)>=30){
-            echo "<script>alert('加選失敗')</script>";
-        }
-        else if($sql3){
+        else if((int)$credit+(int)mysqli_fetch_row($result1)>=30){
             echo "<script>alert('加選失敗')</script>";
         }
         else {
             echo"<script>alert('加選成功')</script>";
-            mysqli_query($sql2,$con);
             $sql2 = "INSERT INTO registration(c_id,s_id) VALUES('c_id','s_id')";
+            mysqli_query($con,$sql2);
+            
             
 
         }  
     }
     else {
-        echo"<script>alert('執行錯誤')</script>"
+        echo"<script>alert('執行錯誤')</script>";
     }
     
     

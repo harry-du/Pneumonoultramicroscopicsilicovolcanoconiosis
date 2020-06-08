5a988b87c5990429ff7e5774ec8a87b848166db5
John Doe <johndoe@example.com>
2020-06-08 13:50:18 +0800

    add improve

--- Code/add.php
+++ Code/add.php
@@ -14,11 +14,12 @@
     $credit =(int) "3";//$_POST['credit'];
     //$passowrd = $_POST['password'];
 
-    $sql1 = " Select  SUM(credit) FROM registration JOIN course WHERE s_id = '$id' AND course.c_id = registration.c_id";//"select s_id,sum(credit) as t_credit from registration group by s_id";
-    $sql3 = " Select (c_id, week, time) FROM time JOIN registration WHERE week = week AND time = time";
-    $sql4 = " Select now_member max_member FROM course WHERE c_id = '$c_id'";
+    $sql1 = " SELECT  SUM(credit) FROM registration JOIN course WHERE s_id = '$id' AND course.c_id = registration.c_id";//"select s_id,sum(credit) as t_credit from registration group by s_id";
+    $sql3 = " SELECT (c_id, week, time) FROM time JOIN registration WHERE week = week AND time = time";
+    $sql4 = " SELECT now_member max_member FROM course WHERE c_id = '$c_id'";
     $sql5 = " SELECT course.c_name FROM registration JOIN course WHERE course.c_id = registration.c_id AND registration.s_id = 'D0606'";
     $sql6 = " SELECT c_name FROM `course`WHERE course.c_id = '$c_id'";
+    $sql7 = "SELECT * FROM (SELECT Day,Time FROM time WHERE Code = '$cod') a INNER JOIN (SELECT time.Day,time.Time FROM time JOIN class on time.Code = class.Code WHERE class.Person_id = '$ac') b WHERE a.Day = b.Day AND a.Time = b.Time; ";
     
     
     $result1 = mysqli_query($con,$sql1);
@@ -26,6 +27,8 @@
     $result4 = mysqli_query($con,$sql4);
     $result5 = mysqli_query($con,$sql5);
     $result6 = mysqli_query($con,$sql6);
+    $result7 = mysqli_query($con,$sql7);
+    
     if(!$result4)
 	{
 		echo ("Error: ".mysqli_error($con));
@@ -40,7 +43,6 @@
     $row2 = mysqli_fetch_assoc($result1);
     $SUM_credit = intval($row2['SUM(credit)']);
     $num1 = mysqli_num_rows($result5);
-
     for($i=0;$i<$num1;$i++)
     {
         $array[$i] = mysqli_fetch_array($result5);
@@ -48,22 +50,22 @@
     
     if ($id!=null){
         if($now_member+1>$max_member){
-            echo "<script>alert('人數已滿')</script>";
+            echo "<script>alert('人數已滿');window.location.href='details.html';</script>";
         }
-        else if($credit+$SUM_credit>=30){
-            echo "<script>alert('加選失敗')</script>";
+        else if($credit+$SUM_credit>30){
+            echo "<script>alert('加選失敗');window.location.href='details.html';</script>";
         }
-        elseif( preg_match($array,$result6) )
-        {
-            echo "<script>alert('課程失敗')</script>";
+        else if(preg_match($array,$result6)){
+            echo "<script>alert('課程失敗');window.location.href='details.html';</script>";
+        }
+        else if($result7 !=null ){
+            echo "<script>alert('衝堂');window.location.href='details.html';</script>";
         }
         else {
-            echo"<script>alert('加選成功')</script>";
+            
+            echo"<script>alert('加選成功');window.location.href='details.html';</script>";
             $sql2 = "INSERT INTO registration(c_id,s_id) VALUES('c_id','s_id')";
             mysqli_query($con,$sql2);
-            
-            
-
         }  
     }
     else {
@@ -71,16 +73,9 @@
     }
     
     
-    //    $sql =  "SELECT * FROM login WHERE sid ='$id' AND pwd='$passowrd'";
-    //    $result = mysqli_query($con,$sql);//執行sql
-    // $rows=mysqli_num_rows($result);//返回一個數值
-    //if($result->num_rows > 0){//0 false 1 true
-    //    header("refresh:0;url=Main.html");
-    //    exit;}
-
-    //else{//如果使用者名稱或密碼有空
-        echo "表單填寫不完整";
-    //}
+   
+        
+    
     
     //確認s_id
     //確認c_id

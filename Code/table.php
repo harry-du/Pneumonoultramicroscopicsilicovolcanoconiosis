e087e9dad33c3605b900edb771b8678e57b808d3
Hsu_Chun_Wei <super20110505@gmail.com>
2020-06-12 14:48:44 +0800

    php update

--- Code/table.php
+++ Code/table.php
@@ -12,11 +12,12 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
    
 </head>
 
 <body>
-    <div class='container'> <table class="table"> <thead style="background-color: #FF8EFF">;
+    <div class='container'> <table class="table"> <thead style="background-color: #FF8EFF">
     <tr><th>&emsp;</th> <th id = 'w1'>星期一</th> <th id = 'w2'>星期二</th> <th id = 'w3'>星期三</th> <th id = 'w4'>星期四</th> <th id = 'w5'>星期五</th> </tr></thead>
     <tbody align = "center"><tr id = '1'> <td>一<br>8:10~9:00</td> <td>&emsp;</td> <td>&emsp;</td> <td>&emsp;</td> <td>&emsp;</td> <td>&emsp;</td></tr>
     <tr id = '2'><td>二<br>9:10~10:00</td> <td id = 'c21'>&emsp;</td> <td>&emsp;</td> <td>&emsp;</td> <td>&emsp;</td> <td>&emsp;</td></tr>
@@ -33,23 +34,30 @@
 </html>
 <?php
     include("connect.php");
-    $s_id = $_SESSION['s_id'];
-    $sql = "SELECT `c_name`,`week`,`time` FROM (course INNER JOIN registration ON (course.c_id = registration.c_id) AND (s_id='$s_id')) INNER JOIN `time` ON(course.c_id = time.c_id)";
+    //$s_id = 'D0606'$_SESSION['s_id'];
+    $sql = "SELECT `c_name`,`week`,`time` FROM (`course` INNER JOIN `registration` ON (course.c_id = registration.c_id) AND (s_id='D0606')) INNER JOIN `time` ON(course.c_id = time.c_id)";
     $result = mysqli_query($con,$sql);
-    $row = mysqli_fetch_array($result);
-    $cname = $row[0];
-    $course = intval($row[2]);
-    echo " <script>
-    if($row[1].indexOf('一')){
-        $('#w1' && '#$course').append($cname);
-    }else if($row[1].indexOf('二')){
-        $('#w2' && '#$course').append($cname);
-    }else if($row[1].indexOf('三')){
-        $('#w3' && '#$course').append($cname);
-    }else if($row[1].indexOf('四')){
-        $('#w4' && '#$course').append($cname);
-    }else if($row[1].indexOf('五')){
-        $('#w5' && '#$course').append($cname);
+    //$array = mysqli_fetch_array($result);
+    $total_fields=mysqli_num_rows($result);
+    for ($i = 0; $i < $total_fields; $i++) {
+        $array = mysqli_fetch_array($result);
+        $cname = $array['c_name'];
+        $cweek = $array['week'];
+        $course = intval($array['time']);
+        echo "<script>
+        if((preg_match($cweek, \"一\"))==1){
+            $('td:eq($course)').append($cname);
+        }else if((preg_match(\"$cweek\", \"二\"))==1){
+            $('td:eq($course)').append($cname);
+        }else if((preg_match(\"$cweek\", \"三\"))==1){
+            $('td:eq($course)').append($cname);
+        }else if((preg_match(\"$cweek\", \"四\"))==1){
+             $('td:eq($course)').append($cname);
+        }else if((preg_match(\"$cweek\", \"五\"))==1){
+            $('td:eq($course)').append($cname);
+        }
+            
+        </script>";
     }
-    </script>";
+    
 ?>
